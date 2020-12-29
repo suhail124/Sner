f1dcb10ea6b76a73efcca1086fba651de9166078
suhail124 <zacksuhail@gmail.com>
2020-12-29 16:08:29 +0530

    Add files via upload
    i've started learning a little bit of backend

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const os = require('os');
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+console.log('Total Memory:' +totalMemory);
+
+//Template String
+//ES6 / ES2015 :ECMAScript 6
+
+console.log(`Total Memory :${totalMemory}`);
+console.log(`Free Memory :${freeMemory}`);
