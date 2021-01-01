757a755a8d88a0523e4d3e19ab512b88ac3ad64e
suhail124 <zacksuhail@gmail.com>
2021-01-01 19:49:32 +0530

    i've been learning node.js

---
+++ newforp/fsaSync/index.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+
+//fs.mkdir('suhail', (err) => {
+//console.log('folder created');
+//});
+
+//fs.writeFile('./suhail/bio.txt', "hello this is suhail", (err) => 
+//{console.log('files created')
+//});
+
+//fs.appendFile("./suhail/bio.txt","\ni've written a statement",(err) =>
+//{console.log("data has been appended");
+//});
+
+//fs.readFile("./suhail/bio.txt",'utf-8',(err,data) => {
+  //  console.log(data);
+//});
+
+//fs.rename("./suhail/bio.txt","./suhail/rename.txt", (err) => {
+//console.log("we've renamed the file here");
+//});
+
+//fs.unlink("./suhail/rename.txt", (err) => {
+ //console.log("the file has been deleted");
+//});
+
+//fs.rmdir('./suhail', (err) => {
+//console.log("THe directory name suhail has been deleted");
+//});
